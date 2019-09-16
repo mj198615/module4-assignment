53f73c290cd1afcb63c3da6fc32e6f8d44bb39c5
Megha Joshi <megha.joshi@mindtree.com>
2019-09-16 12:49:19 +0530

    Module 4 assignment

--- src/data/menudata.service.js
+++ src/data/menudata.service.js
@@ -33,38 +33,3 @@ function MenuDataService($http, ApiBasePath) {
 })();
 
 
-(function () {
-'use strict';
-
-angular.module('data')
-.service('MenuDataService', MenuDataService);
-
-MenuDataService.$inject = ['$http', 'ApiBasePath'];
-function MenuDataService($http, ApiBasePath) {
-  var service = this;
-
-  service.getAllCategories = function() {
-    return $http({
-        method: "GET",
-        url: (ApiBasePath + "/categories.json")
-    }).then(function (response) {
-		  console.log("Success in retrieving : getAllCategories");
-      return response.data;
-    });
-  };
-
-  service.getItemsForCategory = function(categoryShortName) {
-    return $http({
-        method: "GET",
-        url: (ApiBasePath + "/menu_items.json"),
-        params: {
-            category: categoryShortName
-        }
-    }).then(function (response) {
-      console.log("Success in retrieving : getItemsForCategory");
-      return response.data;
-    });
-  };
-}
-
-})();
